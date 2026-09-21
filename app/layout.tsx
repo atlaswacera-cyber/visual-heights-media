c9e822733e4630a323a3a8b5ec332c1c4d530a19
atlaswacera-cyber <atlaswacera@gmail.com>
2026-09-21 12:05:32 -0500

    Optimize artist photo loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" as="image" href="/atlas-wacera.jpg" />
+      </head>
       <body>{children}</body>
     </html>
   );
